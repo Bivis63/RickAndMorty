9be344572dea0ccd334e7e8d374b27e93c01c218
NadezhdaSakal <sakaln88@gmail.com>
2024-02-02 21:19:58 +0300

    ui:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    val fragment_version = "1.6.1"
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+    val nav_version = "2.7.4"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 }
--- app/src/main/java/com/example/rickandmorty/presentation/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.rickandmorty.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.rickandmorty.R
+import com.example.rickandmorty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
