985b54131bba64224200db131d00b6c0dae4182d
Bivis <lumenfan63@mail.ru>
2024-02-05 00:33:44 +0400

    есть баг, проект не собираеться, проблема с версией котлина или с хилт

--- app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersFragment.kt
@@ -1,15 +1,12 @@
 package com.example.rickandmorty.presentation.characters
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.rickandmorty.R
+import androidx.fragment.app.Fragment
 import com.example.rickandmorty.databinding.FragmentCharactersBinding
 import dagger.hilt.android.AndroidEntryPoint
-import java.lang.RuntimeException
-import java.util.zip.Inflater
 
 @AndroidEntryPoint
 class CharactersFragment : Fragment() {
